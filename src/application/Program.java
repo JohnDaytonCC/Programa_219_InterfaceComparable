0c97d549a59fa9ef972325b7c4b8186484d14460
JohnDaytonCC <john.dayton.cc@gmail.com>
2022-03-15 16:40:31 -0300

    Implementação da interface Comparable para comparação entre objetos

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,32 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import entities.Employee;
+
 public class Program {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		List<Employee> list = new ArrayList<>();
+		String path = "E:\\JOHN DAYTON\\CURSOS DE EXTENSÃO\\Java Completo\\in.txt";
+		//String path = "C:\\temp\\in.txt";
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			String employeeCsv = br.readLine();
+			while (employeeCsv != null) {
+				String[] fields = employeeCsv.split(",");		// Separação de string por caractere ",".
+				list.add(new Employee(fields[0], Double.parseDouble(fields[1])));
+				employeeCsv = br.readLine();
+			}
+			Collections.sort(list);
+			for (Employee emp : list) {
+				System.out.println(emp.getName() + ", " + emp.getSalary());
+			}
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
 	}
-
 }
